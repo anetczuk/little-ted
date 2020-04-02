bc0dbeba80b65b70b4416f9ba0f2b7e5ced72be8
anetczuk <anetczuk@o2.pl>
2020-04-02 08:46:26 +0200

    Add script adding shortcut to menu

---
+++ src/configure_menu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+## works both under bash and sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+
+## add udev rule
+AUTOSTART_FILE=~/.local/share/applications/menulibre-littleted.desktop
+
+
+cat > $AUTOSTART_FILE << EOL
+[Desktop Entry]
+Version=1.1
+Type=Application
+Name=Little Ted
+Comment=A small descriptive blurb about this application.
+#Icon=$SCRIPT_DIR/linakdeskapp/gui/img/office-chair-black.png
+Exec=$SCRIPT_DIR/startlittleted
+Path=$SCRIPT_DIR
+Actions=
+Categories=Games;Office;
+StartupNotify=true
+Terminal=false
+
+EOL
+
+
+echo "File created in: $AUTOSTART_FILE"
